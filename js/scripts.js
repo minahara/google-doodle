3b868fa50ab148ed7e84d43be9e9ae2421b11e98
minahara <minahara@vcd-ceb131-12.eastern.ewu.edu>
2017-02-22 09:49:32 -0800

    added svg, random

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-// YOUR SCRIPTS GO HERE
+// randomize image
+
+//store image urls
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+// get random num
+var limit = 3;
+var randNum = Math.floor(Math.random() * max);
+console.log(randNum);
+
+//show image
+//$('img').attr('src', eval('img' + numRand));
