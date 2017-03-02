0fbfefea858b3187bfbc90cd5606c2b9a7c8b894
minahara <minahara@it-mmc16-m14.eastern.ewu.edu>
2017-03-02 14:45:25 -0800

    draw
    got it!
    
    next click event and opacity

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
-// randomize image
+var google = $('svg').drawsvg({
+  duration : 2000,
+  
+});
 
-//store image urls
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
-// get random num
-var limit = 3;
-var randNum = Math.floor(Math.random() * max);
-console.log(randNum);
 
-//show image
-//$('img').attr('src', eval('img' + numRand));
+// Animate SVG points
+google.drawsvg('animate');
