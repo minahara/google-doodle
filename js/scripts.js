f8a7298fbb466921b51d2700cb16e9207bd780fb
minahara <minahara@vcd-ch204-04.eastern.ewu.edu>
2017-03-07 11:14:28 -0800

    Finally!
    oculus repair working!

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ var scar = $('svg').drawsvg({
 
 // Animate SVG points
 scar.drawsvg('animate');
+//random number genorator
+var limit = 3;
+var randNum = Math.floor(Math.random() * limit);
+console.log(randNum);
+
+//Get random body class
+//Change background color
+$('body').addClass('bg' + randNum);
